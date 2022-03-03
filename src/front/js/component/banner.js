4453f03c408716777aa8c7934c0fce8494d145a4
Karla Cuevas <karlacuevas@Karlas-MacBook-Pro.local>
2022-03-02 20:53:27 -0500

    Styled login page and added a banner

---
+++ src/front/js/component/banner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "/Users/karlacuevas/Documents/messaging-app/src/front/styles/banner.scss";
+
+export const Banner = () => {
+	return (
+		<div className="loginJumbo d-block width-100">
+			<h1 className="jumboTitle">
+				Welcome <br />
+				Back.
+			</h1>
+		</div>
+	);
+};
